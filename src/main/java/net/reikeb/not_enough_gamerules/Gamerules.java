026e2d5b8cd6846390b60824d30a91e93d57d809
Max094Reikeb <maxencebekier@gmail.com>
2021-03-25 23:30:33 +0100

    Modified deathExplosion(Time) Gamerule

---
+++ src/main/java/net/reikeb/not_enough_gamerules/Gamerules.java
@@ -0,0 +1,12 @@
+package net.reikeb.not_enough_gamerules;
+
+import net.minecraft.world.GameRules;
+
+public class Gamerules {
+
+    public static final GameRules.RuleKey<GameRules.BooleanValue> DEATH_EXPLOSION = GameRules
+            .register("deathExplosion", GameRules.Category.MISC, NotEnoughGamerules.create(false));
+
+    public static final GameRules.RuleKey<GameRules.IntegerValue> DEATH_EXPLOSION_TIME = GameRules
+            .register("deathExplosionTime", GameRules.Category.MISC, NotEnoughGamerules.create(10));
+}
--- src/main/java/net/reikeb/not_enough_gamerules/NotEnoughGamerules.java
+++ src/main/java/net/reikeb/not_enough_gamerules/NotEnoughGamerules.java
@@ -1,12 +1,15 @@
 package net.reikeb.not_enough_gamerules;
 
+import net.minecraft.world.GameRules;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-
-import net.reikeb.not_enough_gamerules.gamerules.*;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Method;
 
 @Mod(NotEnoughGamerules.MODID)
 public class NotEnoughGamerules {
@@ -20,7 +23,29 @@ public class NotEnoughGamerules {
     public NotEnoughGamerules() {
 
         // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(new DeathExplosion());
+        MinecraftForge.EVENT_BUS.register(new Gamerules());
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public static GameRules.RuleType<GameRules.BooleanValue> create(boolean defaultValue) {
+        try {
+            Method createGameruleMethod = ObfuscationReflectionHelper.findMethod(GameRules.BooleanValue.class, "create", boolean.class);
+            createGameruleMethod.setAccessible(true);
+            return (GameRules.RuleType<GameRules.BooleanValue>) createGameruleMethod.invoke(null, defaultValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static GameRules.RuleType<GameRules.IntegerValue> create(int defaultValue) {
+        try {
+            Method createGameruleMethod = ObfuscationReflectionHelper.findMethod(GameRules.IntegerValue.class, "create", int.class);
+            createGameruleMethod.setAccessible(true);
+            return (GameRules.RuleType<GameRules.IntegerValue>) createGameruleMethod.invoke(null, defaultValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
