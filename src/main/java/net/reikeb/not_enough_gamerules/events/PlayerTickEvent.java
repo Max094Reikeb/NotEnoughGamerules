90dd7f88b221014bcba79730b638b2d37a9d4b3b
Max094Reikeb <maxencebekier@gmail.com>
2021-03-26 16:42:45 +0100

    Fixed SkyHigh procedure

--- src/main/java/net/reikeb/not_enough_gamerules/events/PlayerTickEvent.java
+++ src/main/java/net/reikeb/not_enough_gamerules/events/PlayerTickEvent.java
@@ -16,17 +16,18 @@ public class PlayerTickEvent {
 
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
+        if (event.phase == TickEvent.Phase.END && !event.player.level.isClientSide) {
             PlayerEntity entity = event.player;
             if ((entity.getFoodData().getFoodLevel() < 20)
                     && !entity.level.getLevelData().getGameRules().getBoolean(Gamerules.NATURAL_HUNGER)) {
                 entity.getFoodData().setFoodLevel(20);
             }
             if (entity.getY() < entity.level.getLevelData().getGameRules().getInt(Gamerules.SKY_HIGH)) {
-                for (int i = 0; i < 2000; i++) {
-                    entity.displayClientMessage(new TranslationTextComponent("message.not_enough_gamerules.sky_high_warning"), true);
+                entity.displayClientMessage(new TranslationTextComponent("message.not_enough_gamerules.sky_high_warning"), true);
+
+                if (entity.level.getGameTime() % 200 == 0 && entity.tickCount > 199) {
+                    entity.hurt(new DamageSource("sky_high").bypassArmor(), (float) 10);
                 }
-                entity.hurt(new DamageSource("sky_high").bypassArmor(), (float) 6);
             }
         }
     }
