4f96f521f834014d9515eda880147b78419754e4
FirstMegaGame4 <firstmegagame4@outlook.fr>
2022-03-27 16:33:21 +0200

    I forgot that

--- src/main/java/net/reikeb/notenoughgamerules/mixin/EnderDragonFightMixin.java
+++ src/main/java/net/reikeb/notenoughgamerules/mixin/EnderDragonFightMixin.java
@@ -3,8 +3,11 @@ package net.reikeb.notenoughgamerules.mixin;
 import net.minecraft.entity.boss.dragon.EnderDragonFight;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.GameRules;
+
 import net.reikeb.notenoughgamerules.Gamerules;
+
 import org.objectweb.asm.Opcodes;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
