1fb1f9cadaac6eacc559a8940c2807ca3f8992f3
Max094Reikeb <maxencebekier@gmail.com>
2022-04-03 22:57:30 +0200

    Fixed `villagerConversion` gamerule

--- src/main/java/net/reikeb/not_enough_gamerules/mixin/entity/ZombieMixin.java
+++ src/main/java/net/reikeb/not_enough_gamerules/mixin/entity/ZombieMixin.java
@@ -1,6 +1,5 @@
 package net.reikeb.not_enough_gamerules.mixin.entity;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.entity.monster.Zombie;
 
@@ -15,14 +14,13 @@ import java.util.Random;
 @Mixin(Zombie.class)
 public class ZombieMixin extends EntityMixin {
 
-    @Redirect(method = "killed", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;getDifficulty()Lnet/minecraft/world/Difficulty;", ordinal = 2))
-    private Difficulty removeDifficulty(ServerLevel instance) {
-        return Difficulty.EASY;
-    }
-
     @Redirect(method = "killed", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextBoolean()Z"))
     private boolean removeBoolean(Random instance) {
+        float randomFloat = instance.nextFloat();
         float villagerConversion = (float) this.level.getGameRules().getInt(Gamerules.VILLAGER_CONVERSION) / 100;
-        return (instance.nextFloat() < villagerConversion) || (this.level.getDifficulty() == Difficulty.EASY);
+        if (level.getDifficulty() == Difficulty.NORMAL) {
+            return randomFloat >= villagerConversion;
+        }
+        return randomFloat < villagerConversion;
     }
 }
