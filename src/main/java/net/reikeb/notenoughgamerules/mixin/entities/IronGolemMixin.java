4ab9ee1c49b0e3e0411ca3e14c174733af5c403a
Max094Reikeb <maxencebekier@gmail.com>
2022-04-08 14:33:24 +0200

    Fixed recursive issue

--- src/main/java/net/reikeb/notenoughgamerules/mixin/entities/IronGolemMixin.java
+++ src/main/java/net/reikeb/notenoughgamerules/mixin/entities/IronGolemMixin.java
@@ -12,13 +12,14 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.UUID;
 
 @Mixin(IronGolemEntity.class)
-public abstract class IronGolemMixin extends MobEntityMixin implements IronGolemInterface {
+public abstract class IronGolemMixin extends EntityMixin implements IronGolemInterface {
     @Unique
     public UUID neg$owner;
 
@@ -35,12 +36,15 @@ public abstract class IronGolemMixin extends MobEntityMixin implements IronGolem
 
     @Inject(method = "canTarget", at = @At("HEAD"), cancellable = true)
     private void changeTarget(EntityType<?> type, CallbackInfoReturnable<Boolean> cir) {
-        if ((this.isPlayerCreated() && type == EntityType.PLAYER
-                && (!this.world.getGameRules().getBoolean(Gamerules.ONLY_GOLEMS_OWNER_FRIENDLY)))
-                || (type == EntityType.CREEPER)) {
+        if (this.isPlayerCreated() && type == EntityType.PLAYER
+                && (!this.world.getGameRules().getBoolean(Gamerules.ONLY_GOLEMS_OWNER_FRIENDLY))) {
             cir.setReturnValue(false);
         }
-        cir.setReturnValue(super.canTargetEntity(type));
+    }
+
+    @Redirect(method = "canTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/IronGolemEntity;isPlayerCreated()Z"))
+    private boolean changeIsPlayerCreated(IronGolemEntity instance) {
+        return false;
     }
 
     @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
--- src/main/java/net/reikeb/notenoughgamerules/mixin/entities/MobEntityMixin.java
+++ src/main/java/net/reikeb/notenoughgamerules/mixin/entities/MobEntityMixin.java
@@ -1,6 +1,5 @@
 package net.reikeb.notenoughgamerules.mixin.entities;
 
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.MobEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,11 +13,5 @@ public abstract class MobEntityMixin extends EntityMixin {
 
     @Shadow
     public void tick() {
-    }
-
-    public abstract boolean canTarget(EntityType<?> type);
-
-    public boolean canTargetEntity(EntityType<?> type) {
-        return this.canTarget(type);
     }
 }
