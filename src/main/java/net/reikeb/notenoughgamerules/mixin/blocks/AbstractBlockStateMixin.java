b5599f2079e13f634b566c94f1e1fef93e6f3ebc
FirstMegaGame4 <firstmegagame4@outlook.fr>
2022-04-30 19:54:00 +0200

    Rename GameRule `canPlayerInteractWithBlock` to `canEntityInteractWithBlock` and Finish it. Rename GameRule `canPlayerInteractWithEntity` to `canEntityInteractWithEntity` and Finish it. Rename GameRule `canPlayerInteractWithEntity` to `canEntityInteractWithWorld`and Finish it.

---
+++ src/main/java/net/reikeb/notenoughgamerules/mixin/blocks/AbstractBlockStateMixin.java
@@ -0,0 +1,28 @@
+package net.reikeb.notenoughgamerules.mixin.blocks;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.reikeb.notenoughgamerules.Gamerules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractBlock.AbstractBlockState.class)
+public class AbstractBlockStateMixin {
+    @Inject(method = "onEntityCollision", at = @At("HEAD"), cancellable = true)
+    private void onEntityCollision(World world, BlockPos pos, Entity entity, CallbackInfo ci) {
+        if (!world.getGameRules().getBoolean(Gamerules.CAN_ENTITY_INTERACT_WITH_WORLD)) ci.cancel();
+    }
+    @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
+    private void onUse(World world, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        if (!world.getGameRules().getBoolean(Gamerules.CAN_ENTITY_INTERACT_WITH_BLOCK)) cir.setReturnValue(ActionResult.PASS);
+    }
+}
