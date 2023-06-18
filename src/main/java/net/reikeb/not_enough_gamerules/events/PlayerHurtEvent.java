9e0a979668fa941496a3bf7cdc9297234ee35fba
DerEingerostete <git@dereingerostete.dev>
2023-06-18 22:47:17 +0200

    Removed unnecessary cast to LivingEntity
    Took 36 minutes

--- src/main/java/net/reikeb/not_enough_gamerules/events/PlayerHurtEvent.java
+++ src/main/java/net/reikeb/not_enough_gamerules/events/PlayerHurtEvent.java
@@ -1,6 +1,7 @@
 package net.reikeb.not_enough_gamerules.events;
 
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.player.Player;
@@ -12,33 +13,44 @@ import net.reikeb.not_enough_gamerules.DamageSources;
 import net.reikeb.not_enough_gamerules.Gamerules;
 import net.reikeb.not_enough_gamerules.NotEnoughGamerules;
 
+import java.util.Objects;
+
 @Mod.EventBusSubscriber(modid = NotEnoughGamerules.MODID)
 public class PlayerHurtEvent {
 
     @SubscribeEvent
     public static void onEntityAttacked(LivingAttackEvent event) {
-        if (event != null && event.getEntity() != null) {
-            LivingEntity entity = (LivingEntity) event.getEntity();
-            LivingEntity sourceentity = (LivingEntity) event.getSource().getEntity();
-            GameRules gamerules = entity.level.getLevelData().getGameRules();
-            if ((!gamerules.getBoolean(Gamerules.CAN_HURT_PET_MOBS)) && (sourceentity instanceof Player) &&
-                    (entity instanceof TamableAnimal tamedEntity) && (tamedEntity.isTame()) && (tamedEntity.getOwner() == sourceentity)) {
-                event.setCanceled(event.isCancelable());
-            }
-            if ((!gamerules.getBoolean(Gamerules.PVP)) && (entity instanceof Player) && (sourceentity instanceof Player)) {
-                event.setCanceled(event.isCancelable());
-            }
-            if ((gamerules.getInt(Gamerules.EXPLOSION_DAMAGE) > -1) && (event.getSource().isExplosion())) {
-                event.setCanceled(event.isCancelable());
-                entity.hurt(DamageSources.EXPLOSION, (float) gamerules.getInt(Gamerules.EXPLOSION_DAMAGE));
-            }
-            if ((!gamerules.getBoolean(Gamerules.ANVIL_DAMAGE)) && (event.getSource() == DamageSource.ANVIL)) {
-                event.setCanceled(event.isCancelable());
-            }
-            if ((gamerules.getInt(Gamerules.DRAGON_BREATH_DAMAGE) > -1) && (event.getSource() == DamageSource.DRAGON_BREATH)) {
-                event.setCanceled(event.isCancelable());
-                entity.hurt(DamageSource.DRAGON_BREATH, (float) gamerules.getInt(Gamerules.DRAGON_BREATH_DAMAGE));
-            }
+        if (event == null) return;
+
+        LivingEntity entity = event.getEntity();
+        if (entity == null) return;
+
+        DamageSource source = event.getSource();
+        Entity sourceEntity = source.getEntity(); //May be null
+
+        GameRules gamerules = entity.level.getLevelData().getGameRules();
+        boolean cancelable = event.isCancelable();
+
+        if (!gamerules.getBoolean(Gamerules.CAN_HURT_PET_MOBS) &&
+                sourceEntity instanceof Player &&
+                entity instanceof TamableAnimal tamedEntity &&
+                tamedEntity.isTame() && Objects.equals(tamedEntity.getOwner(), sourceEntity)
+        ) {
+            event.setCanceled(cancelable);
+            return;
+        }
+
+        if (!gamerules.getBoolean(Gamerules.PVP) && entity instanceof Player && sourceEntity instanceof Player) {
+            event.setCanceled(cancelable);
+        } else if (gamerules.getInt(Gamerules.EXPLOSION_DAMAGE) > -1 && source.isExplosion()) {
+            event.setCanceled(cancelable);
+            entity.hurt(DamageSources.EXPLOSION, (float) gamerules.getInt(Gamerules.EXPLOSION_DAMAGE));
+        } else if (!gamerules.getBoolean(Gamerules.ANVIL_DAMAGE) && source == DamageSource.ANVIL) {
+            event.setCanceled(cancelable);
+        } else if (gamerules.getInt(Gamerules.DRAGON_BREATH_DAMAGE) > -1 && source == DamageSource.DRAGON_BREATH) {
+            event.setCanceled(cancelable);
+            entity.hurt(DamageSource.DRAGON_BREATH, (float) gamerules.getInt(Gamerules.DRAGON_BREATH_DAMAGE));
         }
     }
+
 }
